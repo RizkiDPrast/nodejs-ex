d36723e7a328c112b58df368ab2989cdc72bddf4
RizkiDPrast <rizkidwip@gmail.com>
2017-03-16 18:18:10 +0700

    update serverjs on new branch

--- server.js
+++ server.js
@@ -2,40 +2,46 @@
 var express = require('express'),
     fs      = require('fs'),
     app     = express(),
-    eps     = require('ejs'),
+    eps     = require('ejs').renderFile,
     morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
 
-app.engine('html', require('ejs').renderFile);
+const bodyParser= require('body-parser');
+
+app.engine('html', eps);
 app.use(morgan('combined'))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+app.use(bodyParser.urlencoded({extended: true}))
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || process.env.RIZKIPORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || process.env.RIZKIIP || 'localhost',
+    mongoHost =  '127.0.0.1',
+    mongoPort =  27017,
+    mongoDatabase = 'user',
+    mongoUser, mongoPassword,
+    mongoURL = 'mongodb://' + mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    if(process.env.OPENSHIFT_MONGODB_DB_URL){
+       mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+      } else if (process.env.DATABASE_SERVICE_NAME) {
+      var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+          mongoHost = process.env[mongoServiceName + '_SERVICE_HOST']
+          mongoPort = process.env[mongoServiceName + '_SERVICE_PORT']
+          mongoDatabase = process.env[mongoServiceName + '_DATABASE']
+          mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+          mongoUser = process.env[mongoServiceName + '_USER'];
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
+      if (mongoHost && mongoPort && mongoDatabase) {
+          mongoURL = 'mongodb://';
+        if (mongoUser && mongoPassword) {
+          mongoURL += mongoUser + ':' + mongoPassword + '@';
+        }
+          mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+      }
     }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
-}
-var db = null,
-    dbDetails = new Object();
+var db = null;
 
 var initDb = function(callback) {
   if (mongoURL == null) return;
@@ -91,6 +97,67 @@ app.get('/pagecount', function (req, res) {
   }
 });
 
+app.get('/post', function (req, res) {
+  //res.status('200').send('used for api only')
+   res.sendFile("./views/inputguest.html", {root:__dirname})
+});
+
+app.post('/sendguest', function (req, res) {
+  var name = req.body.name || "unknown",
+      phone = req.body.phone || '(empty)',
+      comment = req.body.comment || 'none';
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){console.log('error connectiong to mongodb ' + err)});
+  }
+  if (db) {
+    var g = db.collection('guest');
+        g.insert({
+          name: name,
+          phone: phone,
+          comment: comment
+        }, function(err, result) {
+          console.log('result:' + result);
+          g.find({name:name}).toArray(function(err, r){
+            console.log('r:' + r);
+          })
+          res.status(200).jsonp({"success": "true"});
+        })
+  } else {
+    res.status(400).jsonp({"success": "false"});
+  }
+});
+
+app.get('/env', function (req, res) {
+  console.log(process.env)
+  res.json(JSON.stringify(process.env))
+});
+
+app.get('/guestlist', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var g = db.collection('guest');
+        g.find().toArray(function(err, doc) {
+          if (err) {
+            res.status(500).send('Ops did not work');
+          }
+          console.log(doc)
+          res.send('myguest:' + JSON.stringify(doc));
+        })
+  } else {
+    res.send('none');
+  }
+});
+
+app.all('*', function(req, res) {
+  res.send('Unauthorized attemp')
+})
+
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
@@ -103,5 +170,6 @@ initDb(function(err){
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
+console.log('Mongo server running on %s', mongoURL);
 
 module.exports = app ;
